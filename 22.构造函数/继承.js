a179299c5591d31743f295ada824322092524809
waihoyu <13701343809@qq.com>
2019-01-12 23:54:10 +0800

    inheritance

---
+++ 22.构造函数/继承.js
@@ -0,0 +1,18 @@
+function Animal() {
+    this.eat = function () {
+        console.log('Animal eat');
+    }
+}
+
+function Dog() {
+    this.bark = function () {
+        console.log('Dog bark');
+    }
+}
+
+Dog.prototype = new  Animal()
+
+var hashiqi = new Dog()
+
+hashiqi.bark()
+hashiqi.eat()
