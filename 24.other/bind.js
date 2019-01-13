0d5a771b03a63c1c023304c0538af7e5008ff4be
waihoyu <13701343809@qq.com>
2019-01-14 03:41:05 +0800

    apply bind

--- 24.other/bind.js
+++ 24.other/bind.js
@@ -3,13 +3,13 @@
 Function.prototype.bind1 = function(context){
     //保存this，也就是调用bind的那个函数
     var self = this;
-    console.log(arguments)
     var args = [].slice.call(arguments,1);
     console.log(args)
     return function(){
         //将第一次的参数与此次传入的参数合并，调用self函数
         console.log(arguments)
-        return self.apply(context,args.concat([].slice.call(arguments)));
+        // return self.apply(context,args.concat([].slice.call(arguments)));
+        return self.apply(context,([].slice.call(arguments)));
     }
 }
 
@@ -18,6 +18,6 @@ var test =  function (name) {
     console.log('test',this)
 }
 
-var result = test.bind1({x:100})
+var result = test.bind1({x:100},2,3)
 
 result(1)
